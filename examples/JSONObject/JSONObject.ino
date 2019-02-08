fc5ef8542459e36389fc7050c915c89a4040a08b
Sandeep Mistry <s.mistry@arduino.cc>
2019-02-08 10:17:52 -0500

    Add initial JSON object example

---
+++ examples/JSONObject/JSONObject.ino
@@ -0,0 +1,31 @@
+#include <JSON.h>
+
+const char input[] = "{\"result\":true,\"count\":42,\"foo\":\"bar\"}";
+
+var obj;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  obj = JSON.parse(input);
+
+  Serial.println((bool) obj["result"]);
+  Serial.println((int) obj["count"]);
+  Serial.println((double) obj["count"]);
+  Serial.println((const char*) obj["foo"]);
+
+  Serial.println(JSON.stringify(obj));
+
+  obj["result"] = false;
+  obj["count"] = 4242;
+  obj["foo"] = "ciao";
+  obj["hello"] = "world";
+  obj["true"] = true;
+  obj["x"] = 42;
+
+  Serial.println(obj);
+}
+
+void loop() {
+}
